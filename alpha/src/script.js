7e44db52c93d4faedea89fd0b75b5df65d04a88a
Harry Zhang <zh.harry@yahoo.ca>
2018-04-14 10:32:35 -0400

    New version frontpage

---
+++ alpha/src/script.js
@@ -0,0 +1,144 @@
+function getHours() {
+	var name = document.getElementById("getHoursStudentName").value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/sched.php?action=getHours&name="+name, false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	var contentText = "<p><b>Student: </b>"+data["name"]+"</p>";
+	if (data["hours"] == -1)
+		contentText += "<p>No Records as of date!</p>";
+	else
+		contentText += "<p><b>Total Hours: </b>"+data["hours"]+"</p>";
+	document.getElementById("getHoursStudentContent").innerHTML = contentText;
+}
+
+function getAllHours() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/sched.php?action=getAllHours", false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	console.log(data);
+	if ("error" in data)
+		alert(data["error"]);
+	else {
+		var contentText = "<table class='displayAllTable'><tr><td><b>Student</b></td><td><b>Hours</b></td></tr>"
+		
+		for (var key in data["data"]) {
+			contentText += "<tr><td>"+key+"</d></td><td>";
+			contentText += data["hours"] == -1 ? "No Records as of date!" : data["data"][key];
+			contentText += "</td></tr>";
+		}
+		document.getElementById("getHoursStudentContent").innerHTML = contentText + "</table>";
+	}
+}
+
+function setActiveSched() {
+	var req = "";
+	req += "&MON-morning="+document.getElementById("mon-morn").value;
+	req += "&MON-lunch="+document.getElementById("mon-lunch").value;
+	req += "&MON-afternoon="+document.getElementById("mon-after").value;
+	req += "&TUE-morning="+document.getElementById("tue-morn").value;
+	req += "&TUE-lunch="+document.getElementById("tue-lunch").value;
+	req += "&TUE-afternoon="+document.getElementById("tue-after").value;
+	req += "&WED-morning="+document.getElementById("wed-morn").value;
+	req += "&WED-lunch="+document.getElementById("wed-lunch").value;
+	req += "&WED-afternoon="+document.getElementById("wed-after").value;
+	req += "&THU-morning="+document.getElementById("thu-morn").value;
+	req += "&THU-lunch="+document.getElementById("thu-lunch").value;
+	req += "&THU-afternoon="+document.getElementById("thu-after").value;
+	req += "&FRI-morning="+document.getElementById("fri-morn").value;
+	req += "&FRI-lunch="+document.getElementById("fri-lunch").value;
+	req += "&FRI-afternoon="+document.getElementById("fri-after").value;
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/activeSched.php?action=set"+req, false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	if ("error" in data)
+		alert(data["error"]);
+	else
+		alert("Updated!");
+}
+
+function pushSched() {	
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/activeSched.php?action=push", false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	if ("error" in data)
+		alert(data["error"]);
+	else {
+		alert("Pushed!");
+		reload();
+	}
+}
+
+function pushThisSched() {	
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/activeSched.php?action=pushThis", false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	if ("error" in data)
+		alert(data["error"]);
+	else {
+		alert("Pushed!");
+		reload();
+	}
+}
+
+function completeShift(id) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/sched.php?action=completeShift&id="+id, false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	if ("error" in data)
+		alert(data["error"]);
+	else {
+		alert("Signed on!");
+		reload();
+	}
+}
+
+function loadActiveSched() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/activeSched.php?action=fetch", false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	document.getElementById("mon-morn").innerHTML = data["MON"]["morning"];
+	document.getElementById("mon-lunch").innerHTML = data["MON"]["lunch"];
+	document.getElementById("mon-after").innerHTML = data["MON"]["afternoon"];
+	document.getElementById("tue-morn").innerHTML = data["TUE"]["morning"];
+	document.getElementById("tue-lunch").innerHTML = data["TUE"]["lunch"];
+	document.getElementById("tue-after").innerHTML = data["TUE"]["afternoon"];
+	document.getElementById("wed-morn").innerHTML = data["WED"]["morning"];
+	document.getElementById("wed-lunch").innerHTML = data["WED"]["lunch"];
+	document.getElementById("wed-after").innerHTML = data["WED"]["afternoon"];
+	document.getElementById("thu-morn").innerHTML = data["THU"]["morning"];
+	document.getElementById("thu-lunch").innerHTML = data["THU"]["lunch"];
+	document.getElementById("thu-after").innerHTML = data["THU"]["afternoon"];
+	document.getElementById("fri-morn").innerHTML = data["FRI"]["morning"];
+	document.getElementById("fri-lunch").innerHTML = data["FRI"]["lunch"];
+	document.getElementById("fri-after").innerHTML = data["FRI"]["afternoon"] == "" ? "<b>CLOSED</b>" : data["FRI"]["afternoon"];
+}
+
+function addHours(id) {
+	var hours = document.getElementById("addHoursStudent"+id).value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "api/sched.php?action=addHours&id="+id+"&hours="+hours, false);
+	xhttp.send();
+	var data = new Array(JSON.parse(xhttp.responseText))[0];
+	
+	if ("error" in data)
+		alert(data["error"]);
+	else
+		alert("Bonus Hours changed!");
+}
+
+function reload() {
+	window.location.reload(true);
+}
--- src/script.js
+++ src/script.js
@@ -1,144 +1,66 @@
-function getHours() {
-	var name = document.getElementById("getHoursStudentName").value;
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/sched.php?action=getHours&name="+name, false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	var contentText = "<p><b>Student: </b>"+data["name"]+"</p>";
-	if (data["hours"] == -1)
-		contentText += "<p>No Records as of date!</p>";
-	else
-		contentText += "<p><b>Total Hours: </b>"+data["hours"]+"</p>";
-	document.getElementById("getHoursStudentContent").innerHTML = contentText;
-}
+var isAdmin = false; // Check to see if the page is admin or not
+var ajaxRequests = 0; // Used by Loading Screen to check how many active AJAX requests are still ongoing
+var uniqueID = 371; // Used in Admin page dialog generation to provide each div with a unique ID
 
-function getAllHours() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/sched.php?action=getAllHours", false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	console.log(data);
-	if ("error" in data)
-		alert(data["error"]);
-	else {
-		var contentText = "<table class='displayAllTable'><tr><td><b>Student</b></td><td><b>Hours</b></td></tr>"
-		
-		for (var key in data["data"]) {
-			contentText += "<tr><td>"+key+"</d></td><td>";
-			contentText += data["hours"] == -1 ? "No Records as of date!" : data["data"][key];
-			contentText += "</td></tr>";
+// Constants
+var hours = {Morning: 0.5, Lunch: 0.5, Afternoon: 1};
+var dateIDs = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function indexLoad(update = false) {
+	var date = new Date().getDay();
+	// Sat and Sun Report as Monday
+	if (date == 0 || date == 6)
+		date = 1;
+	
+	// Query Data
+	if (update) {
+		ajax("schedule/get.php", {JustToday: ""}, getToday);
+		ajax("schedule/get.php", {}, getWeek, ["Update"]);
+	} else {
+		// Create collapsible elements for each weekday
+		var coll = document.getElementsByClassName("collapsible");
+		for (var i = 0; i < coll.length; i++) {
+			// Allow Weekly Collapsible for all days but "Today"
+			// Under admin, today is non-collapsible as the data is shown in the "Today" section
+			if (coll[i].innerHTML.trim() != dateIDs[date] || !isAdmin) {
+				coll[i].addEventListener("click", function() {
+					var elem = this.nextElementSibling;
+					this.classList.toggle("opened");
+					if (elem.style.maxHeight) {
+						elem.style.maxHeight = null;
+						if (elem.id != "friday") {
+							elem.style.borderBottom = "1px solid #dedede";
+						}
+					} else {
+						elem.style.maxHeight = elem.scrollHeight + "px";
+						elem.style.borderBottom = "0px solid #dedede";
+					}
+				});
+			// Change ::after text to redirect attention to above
+			} else {
+				if (isAdmin) {
+					coll[i].classList.toggle("notCollapsible");
+					coll[i].innerHTML += "<small>&nbsp; (See Today)</small>";
+				}
+			}
 		}
-		document.getElementById("getHoursStudentContent").innerHTML = contentText + "</table>";
+		ajax("schedule/get.php", {JustToday: ""}, getToday);
+		ajax("schedule/get.php", {}, getWeek);
 	}
+	
+	// Finish Up
+	if (isAdmin)
+		initializeDialogs();
+}
+// Set isAdmin to true, so that every other function will load admin elements
+function adminLoad() {
+	isAdmin = true;
 }
 
-function setActiveSched() {
-	var req = "";
-	req += "&MON-morning="+document.getElementById("mon-morn").value;
-	req += "&MON-lunch="+document.getElementById("mon-lunch").value;
-	req += "&MON-afternoon="+document.getElementById("mon-after").value;
-	req += "&TUE-morning="+document.getElementById("tue-morn").value;
-	req += "&TUE-lunch="+document.getElementById("tue-lunch").value;
-	req += "&TUE-afternoon="+document.getElementById("tue-after").value;
-	req += "&WED-morning="+document.getElementById("wed-morn").value;
-	req += "&WED-lunch="+document.getElementById("wed-lunch").value;
-	req += "&WED-afternoon="+document.getElementById("wed-after").value;
-	req += "&THU-morning="+document.getElementById("thu-morn").value;
-	req += "&THU-lunch="+document.getElementById("thu-lunch").value;
-	req += "&THU-afternoon="+document.getElementById("thu-after").value;
-	req += "&FRI-morning="+document.getElementById("fri-morn").value;
-	req += "&FRI-lunch="+document.getElementById("fri-lunch").value;
-	req += "&FRI-afternoon="+document.getElementById("fri-after").value;
-	
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/activeSched.php?action=set"+req, false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	if ("error" in data)
-		alert(data["error"]);
+// Loading Screen
+console.log(setInterval(function() {
+	if (ajaxRequests == 0)
+		document.getElementById("loading").style.display = "none";
 	else
-		alert("Updated!");
-}
-
-function pushSched() {	
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/activeSched.php?action=push", false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	if ("error" in data)
-		alert(data["error"]);
-	else {
-		alert("Pushed!");
-		reload();
-	}
-}
-
-function pushThisSched() {	
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/activeSched.php?action=pushThis", false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	if ("error" in data)
-		alert(data["error"]);
-	else {
-		alert("Pushed!");
-		reload();
-	}
-}
-
-function completeShift(id) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/sched.php?action=completeShift&id="+id, false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	if ("error" in data)
-		alert(data["error"]);
-	else {
-		alert("Signed on!");
-		reload();
-	}
-}
-
-function loadActiveSched() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/activeSched.php?action=fetch", false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	document.getElementById("mon-morn").innerHTML = data["MON"]["morning"];
-	document.getElementById("mon-lunch").innerHTML = data["MON"]["lunch"];
-	document.getElementById("mon-after").innerHTML = data["MON"]["afternoon"];
-	document.getElementById("tue-morn").innerHTML = data["TUE"]["morning"];
-	document.getElementById("tue-lunch").innerHTML = data["TUE"]["lunch"];
-	document.getElementById("tue-after").innerHTML = data["TUE"]["afternoon"];
-	document.getElementById("wed-morn").innerHTML = data["WED"]["morning"];
-	document.getElementById("wed-lunch").innerHTML = data["WED"]["lunch"];
-	document.getElementById("wed-after").innerHTML = data["WED"]["afternoon"];
-	document.getElementById("thu-morn").innerHTML = data["THU"]["morning"];
-	document.getElementById("thu-lunch").innerHTML = data["THU"]["lunch"];
-	document.getElementById("thu-after").innerHTML = data["THU"]["afternoon"];
-	document.getElementById("fri-morn").innerHTML = data["FRI"]["morning"];
-	document.getElementById("fri-lunch").innerHTML = data["FRI"]["lunch"];
-	document.getElementById("fri-after").innerHTML = data["FRI"]["afternoon"] == "" ? "<b>CLOSED</b>" : data["FRI"]["afternoon"];
-}
-
-function addHours(id) {
-	var hours = document.getElementById("addHoursStudent"+id).value;
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", "api/sched.php?action=addHours&id="+id+"&hours="+hours, false);
-	xhttp.send();
-	var data = new Array(JSON.parse(xhttp.responseText))[0];
-	
-	if ("error" in data)
-		alert(data["error"]);
-	else
-		alert("Bonus Hours changed!");
-}
-
-function reload() {
-	window.location.reload(true);
-}
+		document.getElementById("loading").style.display = "inherit";
+}, 10));
