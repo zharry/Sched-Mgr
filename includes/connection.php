179ab2f7e70fac6c521f7961b6a330bf13dc95bd
Harry Zhang <zh.harry@yahoo.ca>
2018-08-25 12:52:20 -0400

    New SQL Credential Format

--- includes/connection.php
+++ includes/connection.php
@@ -6,8 +6,7 @@ $whitelist = array( '127.0.0.1','::1' );
 if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
 	$conn = mysqli_connect("localhost", "root", "", "schedmgr-new");
 } else {
-	require_once('/etc/mysql-creds/mysql-creds.php');
-	$conn = mysqli_connect($project_sm["host"], $project_sm["user"], $project_sm["pass"], $project_sm["data"]);
+	$conn = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER_SCHEDMGR'), getenv('MYSQL_PASS_SCHEDMGR'), "schedmgr");
 }
 
 if (!$conn) {
